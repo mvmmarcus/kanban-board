aa624e7af3cdce266bfbc837c7130a7532d3632b
Marcus Vinicius Marques <marcus.marques@wisereducacao.com>
2022-02-22 03:41:42 -0300

    feat: update theme

--- FRONT/src/styles/theme.ts
+++ FRONT/src/styles/theme.ts
@@ -16,8 +16,16 @@ export default {
     },
   },
   colors: {
-    primary: "#FFFFFF",
-    secondary: "#000000",
+    primary: "#F231A5",
+    secondary: "#3CD3C1",
+    mainBg: "#06092B",
+    lightBg: "#F2F2F2",
+    white: "#FAFAFA",
+    black: "#030517",
+    lightGray: "#EAEAEA",
+    gray: "#8F8F8F",
+    darkGray: "#2E2F42",
+    red: "#FF6347",
   },
   spacings: {
     xxsmall: "0.8rem",
