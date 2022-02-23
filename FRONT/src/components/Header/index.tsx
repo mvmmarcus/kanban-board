29320ecf82e17b516e338b3ecf10b0050c3adc10
Marcus Vinicius Marques <marcus.marques@wisereducacao.com>
2022-02-23 10:21:28 -0300

    feat: add header and heading components

---
+++ FRONT/src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Wrapper } from "./styles";
+
+const Header = () => {
+  return (
+    <Wrapper>
+      <h1>Kanban Board</h1>
+    </Wrapper>
+  );
+};
+
+export default Header;
